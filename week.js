e8ca740bcf0e39d1a56d521371374bf835dd58f4
Liligon <liliana87gonzalez@gmail.com>
2023-01-31 02:09:39 -0300

    agrega week

---
+++ week.js
@@ -0,0 +1,37 @@
+const weekDays = ["lunes", "martes", "miercoles", "jueves", "viernes", "sabado", "domingo"];
+
+function dias(numeros) {
+    switch (numeros) {
+        case 1:
+            console.log(weekDays[0]);
+            break;
+        case 2:
+            console.log(weekDays[1]);
+            break;
+        case 3:
+            console.log(weekDays[2]);
+            break;
+        case 4:
+            console.log(weekDays[3]);
+            break;
+        case 5:
+            console.log(weekDays[4]);
+            break;
+        case 6:
+            console.log(weekDays[5]);
+            break;
+        case 7:
+            console.log(weekDays[6]);
+            break;
+    }
+}
+
+
+
+
+
+
+
+
+
+
